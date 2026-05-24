155a1402488271316f5ce27c37f3ace675647059
Alexandre Aksenov <alexander1aksenov@gmail.com>
2026-05-24 09:57:55 +0200

    Added questions 2,3

--- main.sql
+++ main.sql
@@ -11,7 +11,7 @@ WHERE
 	rating is null;
 -- -> nothing. Replacement shout have no effect.	
 	
--- TOCHECK: заменяет NULL на 0
+-- TOCHECK (on an auxiliary table "movies_with_null"): заменяет NULL на 0
 update "movies"
 set rating = 0
 where rating is null;
@@ -32,3 +32,59 @@ select
 from movies m 
 where m.movie_id is null
 -- -> nothing, as all movies had a rating in initial data.
+
+-- Q2. название фильма и округленное вверх значение рейтинга до ближайшего целого числа.
+select
+	title,
+	ceil(rating) as ceil_rating
+from 
+	movies;
+-- 30 x 2
+-- -> res/tile_rating.csv
+
+-- Q3. The list of clients, who registered last month.
+
+-- First interpretation of the question: during the last month until today (2026)
+select 
+	customer_id,
+	first_name,
+	last_name,
+	registration_date
+from customers c 
+where c.registration_date between now() - interval '1 month' and now() 
+;
+-- -> nothing, which is expected: all data in the table is historical, the Database was assembled in 2022.
+
+
+
+-- Промежуточная таблица: вывести данные одного пользователя + сколько времени прошло от его регистрации до последней
+select 
+	customer_id,
+	first_name,
+	last_name,
+	registration_date,
+	MAX(registration_date) over () as latest,
+	age(MAX(registration_date) over (), registration_date) as dur_before_latest
+from customers c
+;
+
+-- Query for the 2nd interpretation
+select
+	customer_id,
+	first_name,
+	last_name,
+	registration_date,
+	latest,
+	dur_before_latest
+from (select 
+		customer_id,
+		first_name,
+		last_name,
+		registration_date,
+		MAX(registration_date) over () as latest,
+		age(MAX(registration_date) over (), registration_date) as dur_before_latest
+	from customers c)
+where dur_before_latest < interval '1 month'
+;
+-- -> 1 row x 6 cols 
+-- id=12 , Mila Harris, registration date = latest = '2022-12-20'
