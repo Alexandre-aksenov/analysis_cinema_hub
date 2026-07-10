efe4b84d7854f678803b97050c6944feecde51e8
Alexandre Aksenov <alexander1aksenov@gmail.com>
2026-07-10 22:43:55 +0200

    Created folder ./src/stored_proc
    1 file for the 1st commit

---
+++ src/stored_proc/q1.sql
@@ -0,0 +1,37 @@
+-- 1. хранимая процедура ,
+-- которая добавляет новый фильм в таблицу 'movies', но только если фильма с таким названием и годом выпуска еще нет в базе данных.
+
+create or replace procedure AddNewMovie(
+	p_title VARCHAR(255),
+    p_release_year INT default NULL,
+    p_genre VARCHAR(100) default NULL,
+    p_rating DECIMAL(2,1) default NULL,
+    p_duration INT default NULL,
+    p_description TEXT default NULL,
+    p_additional_info JSONB default NULL
+)
+language plpgsql
+as $$
+declare
+	r_existing_movie record;
+BEGIN
+	-- test whether a new movie is being added
+	for r_existing_movie in
+		SElECT
+			m.title,
+			m.release_year
+		FROM movies m
+		where m.title = p_title AND m.release_year = p_release_year
+  	loop
+		-- Exception if the movie already exists
+		raise exception 'AddNewMovie: the movie % already exists', p_title;
+	end loop
+	;
+
+	-- insert the movie
+	insert into movies (title, release_year, genre, rating, duration, description, additional_info) VALUES
+	(p_title, p_release_year, p_genre, p_rating, p_duration, p_description, p_additional_info);
+END;
+$$;
+
+-- Test on a new table: test_q1.txt
